606b42e6e05782e98b4b8657f6372542d091d7e6
Matthias Koenig <konigmatt@googlemail.com>
2015-06-05 10:50:36 +0200

    Updated information for JSBML dependencies

--- cy3sbml/lib/jsbml_maven_repo.sh
+++ cy3sbml/lib/jsbml_maven_repo.sh
@@ -1,13 +1,16 @@
-# get files and install into local repositories
+# install the maven dependencies in local repository
 
 # spi-full-0.2.4
-wget http://sourceforge.net/p/jsbml/code/HEAD/tree/trunk/core/lib/spi-full-0.2.4.jar?format=raw -O spi-full-0.2.4.jar
+# wget http://sourceforge.net/p/jsbml/code/HEAD/tree/trunk/core/lib/spi-full-0.2.4.jar?format=raw -O spi-full-0.2.4.jar
 mvn install:install-file -DgroupId=cysbml-temp -DartifactId=spi-full -Dversion=0.2.4 -Dfile=spi-full-0.2.4.jar -Dpackaging=jar -DgeneratePom=true
 
 # jigsaw-dateParser-0.1
-wget http://sourceforge.net/p/jsbml/code/HEAD/tree/trunk/core/lib/jigsaw-dateParser.jar?format=raw -O jigsaw-dateParser-0.1.jar
+# wget http://sourceforge.net/p/jsbml/code/HEAD/tree/trunk/core/lib/jigsaw-dateParser.jar?format=raw -O jigsaw-dateParser-0.1.jar
 mvn install:install-file -DgroupId=cysbml-temp -DartifactId=jigsaw-dateParser -Dversion=0.1 -Dfile=jigsaw-dateParser-0.1.jar -Dpackaging=jar -DgeneratePom=true
 
+# JSBML core.jar
+#
+# Necessary to build from the latest development trunk. Checkout the SVN and build the core.jar.
 # checkout jsbml svn
 # cd core
 # ant jar # build the core jar
@@ -16,8 +19,6 @@ mvn install:install-file -DgroupId=cysbml-temp -DartifactId=jigsaw-dateParser -D
 #  	<artifactId>core</artifactId>
 #  	<version>1.0</version>
 #  </dependency>
-
-
 mvn install:install-file -DgroupId=cysbml-temp -DartifactId=core -Dversion=1.0 -Dfile=core.jar -Dpackaging=jar -DgeneratePom=true
 
-mvn install:install-file -DgroupId=cysbml-temp -DartifactId=jsbml-with-dependencies -Dversion=1.0 -Dfile=jsbml-with-dependencies-1.0.jar -Dpackaging=jar -DgeneratePom=true
+# mvn install:install-file -DgroupId=cysbml-temp -DartifactId=jsbml-with-dependencies -Dversion=1.0 -Dfile=jsbml-with-dependencies-1.0.jar -Dpackaging=jar -DgeneratePom=true
