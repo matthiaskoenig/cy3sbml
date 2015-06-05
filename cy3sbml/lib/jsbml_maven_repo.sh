135e08d3e9aa45a3aff2bebd7368a31a391ce6fe
Matthias Koenig <konigmatt@googlemail.com>
2015-06-05 14:43:03 +0200

    Build latest develop version

--- cy3sbml/lib/jsbml_maven_repo.sh
+++ cy3sbml/lib/jsbml_maven_repo.sh
@@ -1,4 +1,8 @@
-# install the maven dependencies in local repository
+# JSBML - local maven dependencies
+# This script installs the libraries in the local maven repository 
+
+# change in the lib directory
+cd $CY3SBML/cy3sbml/lib
 
 # spi-full-0.2.4
 # wget http://sourceforge.net/p/jsbml/code/HEAD/tree/trunk/core/lib/spi-full-0.2.4.jar?format=raw -O spi-full-0.2.4.jar
@@ -9,16 +13,9 @@ mvn install:install-file -DgroupId=cysbml-temp -DartifactId=spi-full -Dversion=0
 mvn install:install-file -DgroupId=cysbml-temp -DartifactId=jigsaw-dateParser -Dversion=0.1 -Dfile=jigsaw-dateParser-0.1.jar -Dpackaging=jar -DgeneratePom=true
 
 # JSBML core.jar
-#
-# Necessary to build from the latest development trunk. Checkout the SVN and build the core.jar.
-# checkout jsbml svn
+# To build from the latest development trunk: Checkout the SVN and build the core.jar.
+# 
 # cd core
-# ant jar # build the core jar
-# <dependency>
-# 	<groupId>org.sbml.jsbml</groupId>
-#  	<artifactId>core</artifactId>
-#  	<version>1.0</version>
-#  </dependency>
+# ant jar              # build the core jar
 mvn install:install-file -DgroupId=cysbml-temp -DartifactId=core -Dversion=1.0 -Dfile=core.jar -Dpackaging=jar -DgeneratePom=true
 
-# mvn install:install-file -DgroupId=cysbml-temp -DartifactId=jsbml-with-dependencies -Dversion=1.0 -Dfile=jsbml-with-dependencies-1.0.jar -Dpackaging=jar -DgeneratePom=true
