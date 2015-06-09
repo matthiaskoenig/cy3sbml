14bfbc54dbc8d61c6745f84ea7b92e44b8003fd9
Matthias Koenig <konigmatt@googlemail.com>
2015-06-09 17:17:56 +0200

    Unittest framework integrated & automatic testing in maven build

---
+++ src/test/java/org/cy3sbml/mapping/OneToManyMappingTest.java
@@ -0,0 +1,47 @@
+package org.cy3sbml.mapping;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class OneToManyMappingTest {
+
+	@Test
+	public void testContainsKey() {
+		//fail("Not yet implemented");
+		
+	}
+
+	/*
+	@Test
+	public void testKeySet() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPut() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetValuesString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetValuesListOfString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateReverseMapping() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+	*/
+
+}
