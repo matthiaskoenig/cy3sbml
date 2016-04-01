fc845d525f872c233fa145b19f1400c213533da3
Matthias Koenig <konigmatt@googlemail.com>
2016-04-01 11:47:40 +0200

    local maven repo for dependencies

--- lib/build_jsbml_jars.sh
+++ lib/build_jsbml_jars.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
-# script for building the latest jsbml core jar.
-# ! register in local maven repository after build for using !
-
+########################################################
+# Script for building latest JSBML (core & packages)
+#
 # JSBML should be checked out and environment variable $JSBMLCODE be set
 # to the location of latest source code, i.e. 
-
+#
 # check if the necessary enviroment variables are set
 # The environment variable can be set via
 # export JSBMLCODE=$HOME/svn/jsbml-code
+#
+# ! After building the local maven repository in lib & pom file
+#   have to be updated manually !
+#
+########################################################
 : "${JSBMLCODE:?The JSBML environment variable must be set to the jsbml-code svn directory.}"
 
 # cy3sbml/lib directory
