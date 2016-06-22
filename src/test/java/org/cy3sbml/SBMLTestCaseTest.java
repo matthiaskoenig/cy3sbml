12f655c41e65346852d099eb2062f01890364c9f
Matthias Koenig <konigmatt@googlemail.com>
2016-06-22 16:04:51 +0200

    updated JSBML with latest bugfixes, fixes #100

--- src/test/java/org/cy3sbml/SBMLTestCaseTest.java
+++ src/test/java/org/cy3sbml/SBMLTestCaseTest.java
@@ -12,7 +12,6 @@ import org.junit.runners.Parameterized.Parameters;
  * 
  * TODO: from where the files
  * TODO: update
- * TOCO: check the skipped
  */
 @RunWith(value = Parameterized.class)
 public class SBMLTestCaseTest{
@@ -25,28 +24,11 @@ public class SBMLTestCaseTest{
 	@Parameters(name= "{index}: {0}")
 	public static Iterable<Object[]> data(){
 		HashSet<String> skip = new HashSet<String>(Arrays.asList(new String[]{
-				"10208-pass-00-08-l2v3.xml",
-				"99220-pass-00-02-sev1-l2v2.xml",
 				"99220-pass-00-01-sev1-l2v1.xml",
+				"99220-pass-00-02-sev1-l2v2.xml",
 				"99220-pass-00-03-sev1-l2v3.xml",
-				"20305-pass-00-11-l2v3.xml",
-				"20305-pass-00-12-l2v3.xml",
-				"20305-pass-00-10-l2v3.xml",
-				"20301-pass-00-04-l2v3.xml",
-				"20302-pass-00-04-l2v3.xml",
-				"20302-pass-00-06-l2v3.xml",
-				"20302-pass-00-05-l2v3.xml",
-				"20303-pass-00-04-l2v3.xml",
-				"20304-pass-00-06-l2v3.xml",
-				"20304-pass-00-05-l2v3.xml",
-				"20304-pass-00-04-l2v3.xml",
-				"layout-20404-pass-00-01-sev1-l3v1.xml",
-				"layout-20402-pass-00-01-sev1-l3v1.xml",
-				"layout-20813-pass-00-01-sev1-l3v1.xml",
-				"10204-pass-00-04-l2v3.xml",
-				"10204-pass-00-13-sev1-l2v5.xml",
-				"10204-pass-00-08-sev1-l3v1.xml"
 		}));
+		skip = null;
 		String filter = "pass";
 		return TestUtils.findResources(TestUtils.SBMLTESTCASES_RESOURCE_PATH, ".xml", filter, skip);
 	}
