b04e81eb262d87c0e671aa3cbf69deab0fabd4a1
Matthias Koenig <konigmatt@googlemail.com>
2016-06-23 19:40:34 +0200

    updated maven install script

--- lib/build_jsbml_jars.sh
+++ lib/build_jsbml_jars.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ########################################################
 # Script for building JSBML (core & packages) from 
-# the repository
+# the repository and install as local dependency.
 #
 # 1. Get the repository code
 #	cd $HOME/git 
@@ -10,27 +10,59 @@
 # 2. Export environment variable
 # 	export JSBMLCODE=$HOME/git/jsbml
 #
-# After building the latest JSBML jars these have to be 
-# updated in the local maven repository with the script
-# 	local_maven_repo.sh
+# After the build of the latest JSBML jars these are updated
+# within the code location.
+# 
+# If version numbers of the JSBML libraries change,
+# these have to be updated in this script.
+#
+# The maven repository files have to be deleted to 
+# to force the update.
 #
 ########################################################
+
+# JSBML code directory
 : "${JSBMLCODE:?The JSBML environment variable must be set to the jsbml-code svn directory.}"
 
-# cy3sbml/lib directory
+# lib directory
 LIBDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-# now update to latest revision
+# update to latest revision
 cd $JSBMLCODE
 git pull 
+
+# clean old build files
 rm -r $JSBMLCODE/build
 
 # build core and extensions
+# core available from $JSBMLCODE/core/build/
+# extensions from $JSBMLCODE/build/
 ant jar 
+cd $LIBDIR
 
-# copy CORE
-cp $JSBMLCODE/core/build/*.jar $LIBDIR
+############################
+CORE_VERSION=1.2-SNAPSHOT
+QUAL_VERSION=2.1-b1
+LAYOUT_VERSION=1.0-b1
+COMP_VERSION=1.0-b1
+FBC_VERSION=1.0-b1
+GROUPS_VERSION=0.4-b1
+DISTRIB_VERSION=0.5
+TIDY_VERSION=1.2.1
+############################
 
-# copy EXTENSIONS
-cp $JSBMLCODE/build/*.jar $LIBDIR
+mvn install:install-file -DgroupId=cy3sbml-dep -DartifactId=jsbml -Dversion=$CORE_VERSION -Dfile=$JSBMLCODE/core/build/jsbml-$CORE_VERSION.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=$DIR -DcreateChecksum=true
 
+mvn install:install-file -DgroupId=cy3sbml-dep -DartifactId=jsbml-qual -Dversion=$QUAL_VERSION -Dfile=$JSBMLCODE/build/jsbml-qual-$QUAL_VERSION.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=$DIR -DcreateChecksum=true
+
+mvn install:install-file -DgroupId=cy3sbml-dep -DartifactId=jsbml-layout -Dversion=$LAYOUT_VERSION -Dfile=$JSBMLCODE/build/jsbml-layout-$LAYOUT_VERSION.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=$DIR -DcreateChecksum=true
+
+mvn install:install-file -DgroupId=cy3sbml-dep -DartifactId=jsbml-comp -Dversion=$COMP_VERSION -Dfile=$JSBMLCODE/build/jsbml-comp-$COMP_VERSION.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=$DIR -DcreateChecksum=true
+
+mvn install:install-file -DgroupId=cy3sbml-dep -DartifactId=jsbml-fbc -Dversion=$FBC_VERSION -Dfile=$JSBMLCODE/build/jsbml-fbc-$FBC_VERSION.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=$DIR -DcreateChecksum=true
+
+mvn install:install-file -DgroupId=cy3sbml-dep -DartifactId=jsbml-groups -Dversion=$GROUPS_VERSION -Dfile=$JSBMLCODE/build/jsbml-groups-$GROUPS_VERSION.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=$DIR -DcreateChecksum=true
+
+mvn install:install-file -DgroupId=cy3sbml-dep -DartifactId=jsbml-distrib -Dversion=$DISTRIB_VERSION -Dfile=$JSBMLCODE/build/jsbml-distrib-$DISTRIB_VERSION.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=$DIR -DcreateChecksum=true
+
+mvn install:install-file -DgroupId=cy3sbml-dep -DartifactId=jsbml-tidy -Dversion=$TIDY_VERSION -Dfile=$JSBMLCODE/build/jsbml-tidy-$TIDY_VERSION.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=$DIR -DcreateChecksum=true
