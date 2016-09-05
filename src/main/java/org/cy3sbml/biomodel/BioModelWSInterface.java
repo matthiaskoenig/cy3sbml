29ebf1587f017b5dc77939385dbf6b20d5658b7f
Matthias Koenig <konigmatt@googlemail.com>
2016-09-05 19:23:38 +0200

    small work on icons

--- src/main/java/org/cy3sbml/biomodel/BioModelWSInterface.java
+++ src/main/java/org/cy3sbml/biomodel/BioModelWSInterface.java
@@ -6,12 +6,10 @@ import java.util.LinkedList;
 import java.util.List;
 
 import org.cy3sbml.ConnectionProxy;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import uk.ac.ebi.biomodels.ws.BioModelsWSClient;
-import uk.ac.ebi.biomodels.ws.BioModelsWSException;
-import uk.ac.ebi.biomodels.ws.SimpleModel;
 
 
 /** 
