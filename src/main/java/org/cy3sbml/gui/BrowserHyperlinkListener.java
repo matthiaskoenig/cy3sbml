2e6a8c0c31ec2a098205079d8235f17e6d3e626c
Matthias König <konigmatt@googlemail.com>
2025-06-03 13:47:48 +0200

    bugfix for broken https navigation links

--- src/main/java/org/cy3sbml/gui/BrowserHyperlinkListener.java
+++ src/main/java/org/cy3sbml/gui/BrowserHyperlinkListener.java
@@ -31,15 +31,15 @@ import org.slf4j.LoggerFactory;
 public class BrowserHyperlinkListener implements WebViewHyperlinkListener{
     private static final Logger logger = LoggerFactory.getLogger(BrowserHyperlinkListener.class);
 
-    public static final String URL_CHANGESTATE = "http://cy3sbml-changestate";
-    public static final String URL_IMPORT = "http://cy3sbml-import";
+    public static final String URL_CHANGESTATE = "https://cy3sbml-changestate";
+    public static final String URL_IMPORT = "https://cy3sbml-import";
 
-    public static final String URL_EXAMPLES = "http://cy3sbml-examples";
-    public static final String URL_BIOMODELS = "http://cy3sbml-biomodels";
-    public static final String URL_HELP = "http://cy3sbml-help";
-    public static final String URL_COFACTOR_NODES = "http://cy3sbml-cofactor";
-    public static final String URL_LOADLAYOUT = "http://cy3sbml-layoutload";
-    public static final String URL_SAVELAYOUT = "http://cy3sbml-layoutsave";
+    public static final String URL_EXAMPLES = "https://cy3sbml-examples";
+    public static final String URL_BIOMODELS = "https://cy3sbml-biomodels";
+    public static final String URL_HELP = "https://cy3sbml-help";
+    public static final String URL_COFACTOR_NODES = "https://cy3sbml-cofactor";
+    public static final String URL_LOADLAYOUT = "https://cy3sbml-layoutload";
+    public static final String URL_SAVELAYOUT = "https://cy3sbml-layoutsave";
 
 
     public static final String URL_SBMLFILE = "http://sbml-file";
@@ -54,18 +54,18 @@ public class BrowserHyperlinkListener implements WebViewHyperlinkListener{
     // Set all the URL actions
     static {
         HashMap<String, String> map = new HashMap<>();
-        map.put("http://cy3sbml-glucose", "/models/Koenig_glucose_v1.xml");
-        map.put("http://cy3sbml-galactose", "/models/Koenig_galactose_31.xml");
-        map.put("http://cy3sbml-HepatoNet1", "/models/HepatoNet1.xml");
-        map.put("http://cy3sbml-e_coli_core", "/models/e_coli_core.xml");
-        map.put("http://cy3sbml-iAB_RBC_283", "/models/iAB_RBC_283.xml");
-        map.put("http://cy3sbml-iIT341", "/models/iIT341.xml");
-        map.put("http://cy3sbml-RECON1", "/models/RECON1.xml");
-        map.put("http://cy3sbml-BIOMD0000000001", "/models/BIOMD0000000001.xml");
-        map.put("http://cy3sbml-BIOMD0000000012", "/models/BIOMD0000000012.xml");
-        map.put("http://cy3sbml-BIOMD0000000016", "/models/BIOMD0000000016.xml");
-        map.put("http://cy3sbml-BIOMD0000000084", "/models/BIOMD0000000084.xml");
-        map.put("http://cy3sbml-hsa04360", "/models/hsa04360.xml");
+        map.put("https://cy3sbml-glucose", "/models/Koenig_glucose_v1.xml");
+        map.put("https://cy3sbml-galactose", "/models/Koenig_galactose_31.xml");
+        map.put("https://cy3sbml-HepatoNet1", "/models/HepatoNet1.xml");
+        map.put("https://cy3sbml-e_coli_core", "/models/e_coli_core.xml");
+        map.put("https://cy3sbml-iAB_RBC_283", "/models/iAB_RBC_283.xml");
+        map.put("https://cy3sbml-iIT341", "/models/iIT341.xml");
+        map.put("https://cy3sbml-RECON1", "/models/RECON1.xml");
+        map.put("https://cy3sbml-BIOMD0000000001", "/models/BIOMD0000000001.xml");
+        map.put("https://cy3sbml-BIOMD0000000012", "/models/BIOMD0000000012.xml");
+        map.put("https://cy3sbml-BIOMD0000000016", "/models/BIOMD0000000016.xml");
+        map.put("https://cy3sbml-BIOMD0000000084", "/models/BIOMD0000000084.xml");
+        map.put("https://cy3sbml-hsa04360", "/models/hsa04360.xml");
         EXAMPLE_SBML = Collections.unmodifiableMap(map);
 
         Set<String> set = new HashSet<>();
