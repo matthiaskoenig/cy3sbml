07a4cdf22a87ca3f105e0a75625e4d41f59a7812
Matthias Koenig <konigmatt@googlemail.com>
2025-09-09 23:04:13 +0200

    added additional tests

--- src/test/java/org/cy3sbml/ols/OLSAccessTest.java
+++ src/test/java/org/cy3sbml/ols/OLSAccessTest.java
@@ -22,6 +22,24 @@ public class OLSAccessTest {
     }
 
     @Test
+    public void getTermCompact() throws Exception {
+        String resourceURI = "http://identifiers.org/GO:0042752";
+        String identifier = RegistryUtilities.getIdentifierFromURI(resourceURI);
+
+        Term term = OLSAccess.getTerm(identifier);
+        assertNotNull(term);
+    }
+
+    @Test
+    public void getTermHttps() throws Exception {
+        String resourceURI = "https://identifiers.org/GO:0042752";
+        String identifier = RegistryUtilities.getIdentifierFromURI(resourceURI);
+
+        Term term = OLSAccess.getTerm(identifier);
+        assertNotNull(term);
+    }
+
+    @Test
     public void termToString() throws Exception {
         String resourceURI = "http://identifiers.org/go/GO:0042752";
         String identifier = RegistryUtilities.getIdentifierFromURI(resourceURI);
